c36f6d158cd48593103e8ca38ccf0964b6831532
CrayonCrayoff <90411579+CrayonCrayoff@users.noreply.github.com>
2023-08-02 07:26:57 +0200

    Small update v0.0.2
    Added scroll function. Fixed some bugs.

--- SpeedrunNotesTool.py
+++ SpeedrunNotesTool.py
@@ -1,6 +1,7 @@
 #Import all the needed libraries and such.
 #For the GUI.
 import tkinter as tk
+import tkinter.scrolledtext as tkscrolled
 from tkinter import ttk
 from PIL import Image, ImageTk, ImageOps
 
@@ -10,18 +11,26 @@ import os
 import threading
 import time
 import re
+import yaml
+
 
 
 #Set up window to display things in.
-root = tk.Tk()
-root.minsize(width=120, height=120)
-root.geometry("520x800+1388+0")
-root.title("SpeedrunNotesTool")
-root.config(background="grey18")
+with open("config.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    
+
+    root = tk.Tk()
+    root.minsize(width=120, height=120)
+    root.geometry(config["screenDimensions"])
+    root.title("SpeedrunScrollableNotesTool")
+    root.config(background="grey18")
+
 
 
 #Finds the current working directory, store it as original working directory for later use.
 origiworkDir = os.getcwd()
+
 
 
 #Generate list of notes, separated by split. End of split is indicated in text file by the string "#####", followed by a newline.
@@ -30,7 +39,17 @@ origiworkDir = os.getcwd()
 temporaryList = []
 splitsNotesList = []
 
-with open('TheNotes.txt', 'r') as file:
+#Add the ReadMe file as the first "splits" notes.
+with open('ReadMe.txt', 'r') as file:
+    textLines = file.readlines()
+    for line in textLines:
+        temporaryList.append(line)
+    splitText = "".join(temporaryList)
+    splitsNotesList.append(splitText)
+    temporaryList = []
+
+#Add the actual notes.
+with open('TheNotes.txt', 'r', encoding="utf-8", errors="ignore") as file:
     textLines = file.readlines()
 
 for line in textLines:
@@ -41,19 +60,25 @@ for line in textLines:
     else:
         temporaryList.append(line)
     
+#Add one final "split" so everything lines up with LiveSplit functionality.
+goodJob = """Great job on finishing your run!\n\nPat yourself on the back, rehydrate with some water, stretch a little.\n
+            \nIf you want to do another run, just press the button for "Next Split" or for "Reset".\n
+            \nOf course you can also call it a day and close the program."""
+splitsNotesList.append(goodJob)
+
 
 #Looks in folder called maps and makes an alphabetized list of the names of all the files in it.
 mapsPath = "maps"
 dirmapsList = os.listdir(mapsPath)
 listMapNames = sorted(dirmapsList)
 
-#Generates a list of all the images called mapsList, which will be used later.
+#Generates a list called mapsList, containing all the file names.
 mapsNameList = []
 for name in listMapNames:
     pathAndName = origiworkDir + "\maps\\" + name
     mapsNameList.append(pathAndName)    
 
-#Starting values for list stuffs and such
+#Starting values for list indeces and such
 currentSplit = 0
 currentMap = -1
 screenWidth = root.winfo_width()
@@ -67,50 +92,78 @@ global finalImage
 
 
 
-#Create the label in the window to display text and images.
-label = tk.Label(root, text=splitsNotesList[0],
+#Create the label in the window to display images.
+label = tk.Label(master=root,
     font=("Consolas",10),
     image="",
-    wraplength=986,
     justify="left",
     anchor="nw",
     foreground="white",
     background="grey18",
+    height=screenHeight,
+    width=screenWidth,
     padx=10,
     pady=10)
-label.place(relx=0.0,
-            rely=0.0,
-            anchor="nw")
+
+#Create the text widget to display text. Allows for scrolling if necessary.
+text = tkscrolled.ScrolledText(master=root,
+    font=("Consolas",10),
+    selectbackground="grey35",
+    bg="grey18",
+    fg="white",
+    wrap=tk.WORD,
+    height = screenHeight,
+    width = screenWidth,
+    relief=tk.FLAT)
+text.vbar.config(width="0")
+text.pack(fill="both", expand="True")
+
+text.insert(tk.END, splitsNotesList[0])
+text.config(state=tk.DISABLED)
 
 
- 
-#Function that's called when text needs to be on screen. 
+
+
+#Call display function to resize textwrap size or image size, depending on current mode.
+def resize(e):
+    #Store the screen's dimensions and position in the YAML config file.
+    latestDimensions = root.winfo_geometry() 
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    config["screenDimensions"] = latestDimensions
+    with open("config.yaml", "w") as f:
+        config = yaml.dump(config, stream=f, default_flow_style=False, sort_keys=False)
+    with open("config.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)    
+    
+    global mode
+    if mode == "Maps":
+        displayMaps()
+    else:
+        pass
+
+
+
+#Show text on screen.
 def displayNotes():
+    #Resizes the text widget to fit the screen.
+    global mode
     global splitsNotesList
     global currentSplit
-    global screenWidth
-    global mode
-
-    screenWidth = root.winfo_width()
-    wraptext = screenWidth - 12
     
-    mode = "Notes"
-    
-    label.config(image="")
-    label.image = "",
-    label.config(text=splitsNotesList[currentSplit % len(splitsNotesList)],
-        image="",
-        justify="left",
-        anchor="nw",
-        wraplength=wraptext,
-        foreground="white",
-        background="grey18")
-    label.place(relx=0.0,
-                rely=0.0)
+    mode = "Notes" 
+    label.pack_forget()
+    text.config(state=tk.NORMAL)
+    text.delete("0.0", tk.END)
+    text.insert(tk.END, splitsNotesList[currentSplit % len(splitsNotesList)])
+    text.config(state=tk.DISABLED)
+    text.pack(fill="both", expand="True")
 
+    print("xview is: ", text.xview())
+    print("yview is: ", text.yview())
 
-    
-#Function that's called when a map needs to be on screen.
+#Needlessly complicated way to display an image on the screen.
 def displayMaps():
     global mapsNameList
     global currentMap
@@ -144,18 +197,45 @@ def displayMaps():
     newHeight = round(newHeight) - 4
     resizedImage = originalImage.resize((newWidth, newHeight))
 
+    #Remove the text widget.
+    text.pack_forget()
+
     #Overwrite old image with new image.    
     finalImage = ImageTk.PhotoImage(resizedImage)
     label.config(image=finalImage)
     label.image = finalImage
-    label.place(relx=0.0,
-                rely=0.0,
-                anchor="nw")
-    
-    print("New Width is: " + str(newWidth))
-    print("New Height is: " + str(newHeight))
-    print(root.winfo_geometry())
+    label.pack(fill="both", expand="True")
 
+
+
+#Waits for a keyboard input, checks for certain keys.
+    #This needs to be updated later, to allow the user to set their own hotkeys. 
+def waitkeyPress():
+    running = True
+    while running:
+        event = keyboard.read_event()
+
+        if event.event_type == keyboard.KEY_UP:
+            if event.name == '0' and event.is_keypad == True:
+                nextSplit()
+            elif event.name == '-' and event.is_keypad == True: 
+                undoSplit()
+            elif event.name == '+' and event.is_keypad == True:
+                skipSplit()
+            elif event.name == 'space':
+                nextMap()
+            elif event.name == 'backspace':
+                previousMap()
+            elif event.name == '/' and event.is_keypad == True:
+                resetAll()
+            elif event.name == 'delete':
+                modeSwap()
+            #Closes the window. Only triggers if the window is in focus, to prevent accidental closing.
+            elif event.name == 'q' or event.name == 'esc':
+                if root.focus_displayof():
+                    root.destroy()
+            else:
+                pass
 
 
 
@@ -204,84 +284,10 @@ def modeSwap():
         
     else:
         pass
-    
-
-#Call display function to resize textwrap size or image size, depending on current mode.
-def resize(e):
-    global mode
-    if mode == "Notes":
-        displayNotes()
-    elif mode == "Maps":
-        displayMaps()
-    else:
-        print("You dun goofed")
+        
 
 
-
-
-
-#Waits for a keyboard input, checks for certain keys.
-    #This needs to be updated later, to allow the user to set their own hotkeys. 
-def waitkeyPress():
-    running = True
-    while running:
-        event = keyboard.read_event()
-
-        if event.event_type == keyboard.KEY_UP:
-            if event.name == '0' and event.is_keypad == True:
-                nextSplit()
-            elif event.name == '-' and event.is_keypad == True: 
-                undoSplit()
-            elif event.name == '+' and event.is_keypad == True:
-                skipSplit()
-            elif event.name == 'space':
-                nextMap()
-            elif event.name == 'backspace':
-                previousMap()
-            elif event.name == 'slash' and event.is_keypad == True:
-                resetAll()
-            elif event.name == 'delete':
-                modeSwap()
-            #Closes the window. Only triggers if the window is in focus, to prevent accidental closing.
-            elif event.name == 'q' or event.name == 'esc':
-                if root.focus_displayof():
-                    root.destroy()
-            else:
-                pass
-
-"""
-    running = True
-    while running:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_UP:
-            if event.name == 'f1':
-                nextSplit()
-            elif event.name == 'f3': 
-                undoSplit()
-            elif event.name == 'f4':
-                skipSplit()
-            elif event.name == 'f5':
-                nextMap()
-            elif event.name == 'f6':
-                previousMap()
-            elif event.name == 'f2':
-                resetAll()
-            elif event.name == 'f8':
-                modeSwap()
-            #Closes the window. Only triggers if the window is in focus, to prevent accidental closing.
-            elif event.name == 'esc' and not event.name == '1':
-                if root.focus_displayof():
-                    root.destroy()
-            else:
-                pass
-"""                
-                
-                
 def main():
-    running = True
-
-    
-    
     # start a seperate thread that waits for a global keyboard input
     t = threading.Thread(target=waitkeyPress, daemon=True)
     t.start()
